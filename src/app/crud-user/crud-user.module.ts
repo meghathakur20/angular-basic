628e42488499a77e2771956dfa5b94e7a05de0cb
meghathakur20 <46532086+meghathakur20@users.noreply.github.com>
2022-12-05 21:16:00 +0530

    1 file modified

--- src/app/crud-user/crud-user.module.ts
+++ src/app/crud-user/crud-user.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CrudUserComponent } from './crud-user.component';
 import { CrudUserService } from './crud-user.service';
+import { CrudUserRoutingModule } from './crud-user.routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    CrudUserRoutingModule
   ],
   declarations: [CrudUserComponent],
   providers:[CrudUserService]
